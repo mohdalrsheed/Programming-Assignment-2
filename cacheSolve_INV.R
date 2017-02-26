824f27d944d76b27abf7e6f603bfad23acb428f1
mohdalrsheed <mohdalrsheed@hotmail.com>
2017-02-26 10:10:59 +0300

    Add files via upload

---
+++ cacheSolve_INV.R
@@ -0,0 +1,14 @@
+cacheSolve <- function(x, ...) 
+  {
+  ## Return the inverse of 'x'
+  inv <- x$getInverse()
+  if(!is.null(inv))
+    {
+    message("getting cached data")
+    return(inv)
+  }
+  data <- x$get()
+  inv <- solve(data)
+  x$setInverse(inv)
+  inv      
+}
